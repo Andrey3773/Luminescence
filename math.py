73a2ca6411b9a4120997f903f14561b9e3bc867f
Andrey3773 <andrejtimofeev311@gmail.com>
2025-03-17 19:32:41 +0300

    done graphs

---
+++ math.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+for i in range(1, 8):
+    number = str(i)
+    spectrum_data = pd.read_csv(
+        f"input_files\sample_{number}.txt",
+        sep=",",
+    )
+
+    wavelength_list = spectrum_data["lambda"].to_list()
+
+    intensity_list = spectrum_data[" I"].to_list()
+    max_intensity = max(intensity_list)
+    norm_intensity = [i / max_intensity for i in intensity_list]
+
+    intensity = np.array(norm_intensity)
+    wavelength = np.array(wavelength_list)
+
+
+    comment = (f'$max(I) = {max_intensity}$\n'
+               f'$\lambda = {wavelength_list[intensity_list.index(max_intensity)]}$')
+
+    plt.figure()
+    plt.title(fr"$I(\lambda)_{number}$")
+    plt.plot(wavelength, intensity, '-', label=r"$I(\lambda)$",)
+    plt.figtext(0.15, 0.8, comment, fontsize=11)
+    plt.xlabel(r"$\lambda$", loc='right')
+    plt.ylabel(r"$I$", loc='top')
+    plt.legend()
+    plt.savefig(f"output_files\graph_{number}.jpg")
